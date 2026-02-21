244e59f7a1c8f213de9aa913fcd34f6363c38111
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2026-02-21 13:29:49 -0300

    visual e code

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function contador() {
     btn++;
     display.innerHTML = btn;
     guardar(btn);
+    recompesar(btn);
 }
 
 function guardar(a) {
@@ -21,5 +22,11 @@ function recuperar() {
             document.querySelector("#contador").innerHTML = btn;
         });
     }
+}
+
+function recompesar(btn) {
+    if (btn >= 220) {document.querySelector("#recompensa").innerHTML = "<img src='https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUybjF6eWgxczhscmZ1dWV1Y3c4cXlkZ2pkMHNndDc3dDZoZWx0dDJvZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/v3Rb7dGuvkmaRi6qei/200w.gif'>";}
+    else if (btn >= 200) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat0.gif'>";} 
+    else if (btn >= 100) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat1.gif'>";}
 }
 
