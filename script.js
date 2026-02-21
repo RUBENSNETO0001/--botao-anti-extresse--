416ac5e0c397046a2445cfb4a3899fc8871edd4e
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2026-02-21 14:01:49 -0300

    infeite
    so coloquei um favicon é adicionei um um gif novo do gato com uma arm

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ function recuperar() {
 }
 
 function recompesar(btn) {
-    if (btn >= 220) {document.querySelector("#recompensa").innerHTML = "<img src='https://media3.giphy.com/media/v1.Y2lkPTZjMDliOTUybjF6eWgxczhscmZ1dWV1Y3c4cXlkZ2pkMHNndDc3dDZoZWx0dDJvZCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/v3Rb7dGuvkmaRi6qei/200w.gif'>";}
-    else if (btn >= 200) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat0.gif'>";} 
-    else if (btn >= 100) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat1.gif'>";}
+    if (btn >= 220) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat2.gif'>";}
+    else if (btn >= 200) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat1.gif'>";} 
+    else if (btn >= 100) {document.querySelector("#recompensa").innerHTML = "<img src='./assents/img/cat0.gif'>";}
 }
 
